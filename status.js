8d43b7ed122cba725e713de1228c6c91840c3a9f
MilanT2004 <118364418+MilanT2004@users.noreply.github.com>
2023-05-19 13:59:37 +0200

    Update status.js

--- status.js
+++ status.js
@@ -15,7 +15,7 @@ app.put('/updateStatus', (req, res) => {
   
     fs.writeFile('./status', JSON.stringify(status), 'utf8', (err) => {
       if (err) {
-        console.error(err);
+        console.log(err);
       } else {
         console.log('Status opgeslagen in het bestand.');
       }
